d0384719e6290ffcf99487d66f2e31f3a764d7cd
Julian Pokrovsky <juliankul@gmail.com>
2023-01-23 16:16:02 +0800

    chore: starting drafting the logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+/// Logging client for nanolog-rs infrastructure
 fn main() {
+}
+
+
+/// Log messages are typically 50-100 bytes long,
+/// so a flash drive with 250 Mbytes/sec bandwidth
+/// can only absorb a few million messages per second.
+fn log() {
     println!("Hello, world!");
 }
