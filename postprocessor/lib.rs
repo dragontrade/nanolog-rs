fd10d0e97b07c1ab207a585026cf9cafe5e05cea
Julian Pokrovsky <juliankul@gmail.com>
2023-01-25 23:04:30 +0800

    chore: postprocessor description

--- postprocessor/lib.rs
+++ postprocessor/lib.rs
@@ -1,1 +1,7 @@
+/// The post-processor or decompressor is a separate application that interprets the binary log file,
+/// recombines the dynamic log data with the static information in the dictionary,
+/// and presents it to the user for consumption.
+///
+/// The post-processor can either be executed as a standalone application
+/// or compiled into another application as a library package.
 fn process() {}
